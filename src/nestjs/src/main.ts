ede01a4ba526463d5e8069dab7f9c531e6d173e2
Alex Cândido <alex.candido.tec@gmail.com>
2024-02-20 20:31:02 -0300

    adicionado estrutura incial de global-config

--- src/nestjs/src/main.ts
+++ src/nestjs/src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { applyGlobalConfig } from './global-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: process.env.ORIGIN,
-  });
+  applyGlobalConfig(app);
   await app.listen(process.env.API_PORT);
 }
 bootstrap();
