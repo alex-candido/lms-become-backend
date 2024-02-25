6350082b6d5ff349c6b4f21e308c147916a26896
Alex Cândido <alex.candido.tec@gmail.com>
2024-02-25 20:09:20 -0300

    adicionado configurações de jest em jest.config.ts

---
+++ jest.config.ts
@@ -0,0 +1,44 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from 'jest';
+
+const config: Config = {
+  displayName: {
+    name: 'nestjs',
+    color: 'magentaBright',
+  },
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\..*spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': '@swc/jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageProvider: 'v8',
+  coverageDirectory: '../__coverage',
+  testEnvironment: 'node',
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '.interface.ts',
+    '-interface.ts',
+    'shared/testing',
+    'shared-module/testing',
+    'validator-rules.ts',
+    '-fixture.ts',
+    '.input.ts',
+    '.d.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
+};
+
+export default config;
